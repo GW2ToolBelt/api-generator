0632a2df849945b056ec8effffa968d2fb16d395
Leon Linhart <themrmilchmann@gmail.com>
2021-01-18 23:30:42 +0100

    fix: fixup schema for /v2/dungeons

--- src/main/kotlin/com/gw2tb/apigen/internal/spec/GW2v2.kt
+++ src/main/kotlin/com/gw2tb/apigen/internal/spec/GW2v2.kt
@@ -657,7 +657,7 @@ internal val GW2v2 = GW2APIVersion {
         supportedQueries(BY_ID, BY_IDS, BY_PAGE)
         schema(record(name = "Dungeon", description = "Information about a dungeon.") {
             CamelCase("id").."ID"(STRING, "the dungeon's ID")
-            "Path"(
+            "Paths"(
                 description = "the dungeon's paths",
                 type = array(record("Path", "Information about a dungeon path.") {
                     CamelCase("id").."ID"(STRING, "the path's ID")
