7ca018d6455392031c2e1c1eca1e1d64b691cf16
Leon Linhart <themrmilchmann@gmail.com>
2022-01-03 21:20:10 +0100

    build: update dokka {1.5.31 => 1.6.10}

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ import org.jetbrains.kotlin.gradle.dsl.*
 plugins {
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.serialization") version "1.6.10"
-    id("org.jetbrains.dokka") version "1.5.31"
+    id("org.jetbrains.dokka") version "1.6.10"
     signing
     `maven-publish`
 }
