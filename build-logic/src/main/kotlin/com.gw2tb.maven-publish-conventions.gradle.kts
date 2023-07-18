c3ebef7af4cdaef11344f8d0874823f13bd6692a
Leon Linhart <themrmilchmann@gmail.com>
2023-07-18 16:47:45 +0200

    build: refactor build logic

--- build-logic/src/main/kotlin/com.gw2tb.maven-publish-conventions.gradle.kts
+++ build-logic/src/main/kotlin/com.gw2tb.maven-publish-conventions.gradle.kts
@@ -20,22 +20,27 @@
  * SOFTWARE.
  */
 import com.gw2tb.build.*
-import com.gw2tb.build.BuildType
 
 plugins {
     id("com.gw2tb.base-conventions")
-    signing
     `maven-publish`
+    signing
 }
 
 publishing {
     repositories {
-        maven {
-            url = uri(deployment.repo)
+        val sonatypeUsername: String? by project
+        val sonatypePassword: String? by project
+        val stagingRepositoryId: String? by project
 
-            credentials {
-                username = deployment.user
-                password = deployment.password
+        if (sonatypeUsername != null && sonatypePassword != null && stagingRepositoryId != null) {
+            maven {
+                url = uri("https://oss.sonatype.org/service/local/staging/deployByRepositoryId/$stagingRepositoryId/")
+
+                credentials {
+                    username = sonatypeUsername
+                    password = sonatypePassword
+                }
             }
         }
     }
@@ -73,6 +78,12 @@ publishing {
 }
 
 signing {
-    isRequired = (deployment.type === BuildType.RELEASE)
+    // Only require signing when publishing to a non-local maven repository
+    setRequired { gradle.taskGraph.allTasks.any { it is PublishToMavenRepository } }
+
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
+
     sign(publishing.publications)
 }
