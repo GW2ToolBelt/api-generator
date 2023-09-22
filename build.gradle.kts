2c7814ec1474ca17a010ac72ea08a3614ed59308
Leon Linhart <themrmilchmann@gmail.com>
2023-09-22 16:06:35 +0200

    build: use Java 21 toolchain

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ plugins {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(20))
+        languageVersion.set(JavaLanguageVersion.of(21))
     }
 
     withJavadocJar()
