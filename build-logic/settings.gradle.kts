93c1a8f4b71ad1bfb4e6f7ffa520b195795cc60f
Leon Linhart <themrmilchmann@gmail.com>
2025-08-12 18:55:07 +0200

    build: centrally declare build logic repos

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -20,3 +20,11 @@
  * SOFTWARE.
  */
 rootProject.name = "build-logic"
+
+dependencyResolutionManagement {
+    repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
+
+    repositories {
+        mavenCentral()
+    }
+}
